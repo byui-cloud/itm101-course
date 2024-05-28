41dea278bc9efab1e3045e235f0640893ed55b89
brochristensen <78576381+brochristensen@users.noreply.github.com>
2024-05-28 09:42:44 -0600

    adding scripts to get files

---
+++ week06/week6.bat
@@ -0,0 +1,9 @@
+curl -O https://byui-cloud.github.io/itm101-course/week06/firestore.rules
+cd public 
+curl -O https://byui-cloud.github.io/itm101-course/week06/cr.html
+curl -O https://byui-cloud.github.io/itm101-course/week06/crd.html
+curl -O https://byui-cloud.github.io/itm101-course/week06/crud.html
+curl -O https://byui-cloud.github.io/itm101-course/week06/favicon.ico
+curl -O https://byui-cloud.github.io/itm101-course/week06/logo.png
+cd ..
+dir
