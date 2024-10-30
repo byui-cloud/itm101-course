c70c6c0070f2bb34d831474494d02277b0235790
brochristensen <78576381+brochristensen@users.noreply.github.com>
2024-10-30 14:30:02 -0600

    simplify instructions

---
+++ week07/addipvarwebui.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+# Get the public IP address using curl
+public_ip=$(curl -s http://checkip.amazonaws.com)
+
+# Define the environment variables with the dynamic IP address
+ollama_host="Environment=\"OLLAMA_HOST=0.0.0.0\""
+ollama_origins="Environment=\"OLLAMA_ORIGINS=${public_ip}:*\""
+
+# Path to the file where you want to append these environment variables
+file_path="/etc/systemd/system/ollama.service" # Update with the correct file path
+
+sed -i "/Environment=\"PATH=\/usr\/local\/sbin:\/usr\/local\/bin:\/usr\/sbin:\/usr\/bin\"/a $ollama_host\n$ollama_origins" "$file_path"
+
+echo "Environment variables have been appended to $file_path."
+systemctl daemon-reload
+systemctl restart ollama
