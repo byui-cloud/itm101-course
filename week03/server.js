ab7eec885a2199ad59ef150a6eced2563eac1163
Rex Barzee <barzeer@byui.edu>
2025-05-05 23:27:09 +0100

    Make the NodeJS server more robust for Google Cloud Platform in week 3.

--- week03/server.js
+++ week03/server.js
@@ -3,39 +3,77 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 8080; // Google Cloud Shell allows previewing this port
+const PORT = 8080;  // Google Cloud Shell allows previewing this port
 
-const KNOWN_FILES = ['/index.html', '/script.js', '/style.css', '/favicon.webp'];
 const MIME_TYPES = {
     html: 'text/html',
-    js: 'text/javascript',
     css: 'text/css',
+    js: 'text/javascript',
+    png: 'image/png',
+    jpg: 'image/jpeg',
+    jpeg: 'image/jpeg',
     webp: 'image/webp'
 };
 
-const server = http.createServer((request, result) => {
-    if (request.url == '/') {
-        request.url = '/index.html';
-    }
 
-    if (KNOWN_FILES.includes(request.url)) {
-        const filePath = path.join(__dirname, request.url.slice(1));
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                result.writeHead(500);
-                result.end(`Error loading ${request.url}`);
-            } else {
-                const suffix = path.extname(filePath).slice(1);
-                const mimeType = MIME_TYPES[suffix];
-                result.writeHead(200, { 'Content-Type': mimeType });
-                result.end(data);
-            }
-        });
-    } else {
-        result.writeHead(404);
-        result.end(`File ${request.url} not found`);
-    }
+const server = http.createServer((request, result) => {
+	function sendError(code, message, internal) {
+		console.log(`${code} ${message}`);
+		if (internal) {
+			console.log($`    {internal}`);
+		}
+		result.writeHead(code);
+		result.end(message);
+	}
+
+
+	if (request.url.indexOf("..") !== -1) {
+		sendError(403, `${request.url} is forbidden`);
+	}
+	else {
+		let filePath = path.join(__dirname, request.url.slice(1));
+		fs.stat(filePath, (err, stats) => {
+			if (err) {
+				const suffix = path.extname(request.url).slice(1);
+				if (suffix.length > 0 && (suffix in MIME_TYPES) === false) {
+					sendError(403, `${request.url} is forbidden`);
+				}
+				else {
+					if (err.code === 'ENOENT') {
+						sendError(404, `Error ${request.url} not found`);
+					}
+					else {
+						sendError(500, `Error finding ${request.url}`, err);
+					}
+				}
+			}
+			else {
+				if (stats.isDirectory()) {
+					filePath = path.join(filePath, 'index.html');
+				}
+				console.log(filePath);
+
+				const suffix = path.extname(filePath).slice(1);
+				if ((suffix in MIME_TYPES) === false) {
+					sendError(403, `${request.url} is forbidden`);
+				}
+				else {
+					fs.readFile(filePath, (err, data) => {
+						if (err) {
+							sendError(500, `Error finding ${request.url}`, err);
+						}
+						else {
+							const mimeType = MIME_TYPES[suffix];
+							result.writeHead(200, { 'Content-Type': mimeType });
+							result.end(data);
+						}
+					});
+				}
+			}
+		});
+	}
 });
+
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
