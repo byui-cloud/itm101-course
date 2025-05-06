90472488c30102ed01611d095117b730db6334be
Rex Barzee <barzeer@byui.edu>
2025-05-07 00:27:56 +0100

    Make the NodeJS server more robust for Google Cloud Platform in week 3.

--- week03/server.js
+++ week03/server.js
@@ -20,58 +20,85 @@ const server = http.createServer((request, result) => {
 	function sendError(code, message, internal) {
 		console.log(`${code} ${message}`);
 		if (internal) {
-			console.log($`    {internal}`);
+			console.log(`    ${internal}`);
 		}
 		result.writeHead(code);
 		result.end(message);
 	}
 
 
-	if (request.url.indexOf("..") !== -1) {
-		sendError(403, `${request.url} is forbidden`);
+	/* Request possibilities and results:
+	 * 1. Request contains .. -> 403
+	 * 2. Request has a suffix and suffix not in MIME_TYPES -> 403
+	 * 3. Request (file or directory) doesn't exist -> 404
+	 * 4. Request exists and is a directory -> Change request to dir/index.html
+	 * 5. Request doesn't have a suffix or suffix isn't in MIME_TYPES -> 403
+	 * 6. Request doesn't exist or can't be read -> 404
+	 * 7. Request has a suffix, suffix is in MIME_TYPES, request exists,
+	 *        request can be read, and request is a file -> 200
+	 */
+	const containsDotDot = (request.url.indexOf("..") !== -1);
+	if (containsDotDot) {
+		sendError(403, `${request.url} is forbidden`);  // 1.
+		return;
 	}
-	else {
-		let filePath = path.join(__dirname, request.url.slice(1));
-		fs.stat(filePath, (err, stats) => {
-			if (err) {
-				const suffix = path.extname(request.url).slice(1);
-				if (suffix.length > 0 && (suffix in MIME_TYPES) === false) {
-					sendError(403, `${request.url} is forbidden`);
-				}
-				else {
-					if (err.code === 'ENOENT') {
-						sendError(404, `Error ${request.url} not found`);
-					}
-					else {
-						sendError(500, `Error finding ${request.url}`, err);
-					}
-				}
+
+	let suffix = path.extname(request.url);
+	const hasSuffix = (suffix.length > 1);
+	if (hasSuffix) {
+		suffix = suffix.slice(1);
+		const suffixInMimeTypes = (suffix in MIME_TYPES);
+		if (! suffixInMimeTypes) {
+			sendError(403, `${request.url} is forbidden`);  // 2.
+			return;
+		}
+	}
+
+	let filePath = path.join(__dirname, request.url.slice(1));
+	fs.stat(filePath, (err, stats) => {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				sendError(404, `Error ${request.url} not found`);  // 3.
 			}
 			else {
-				if (stats.isDirectory()) {
-					filePath = path.join(filePath, 'index.html');
-				}
-				console.log(filePath);
+				sendError(500, `Error finding ${request.url}`, err);
+			}
+		}
+		else {
+			if (stats.isDirectory()) {
+				filePath = path.join(filePath, 'index.html');  // 4.
+			}
+			console.log(filePath);
 
-				const suffix = path.extname(filePath).slice(1);
-				if ((suffix in MIME_TYPES) === false) {
-					sendError(403, `${request.url} is forbidden`);
-				}
-				else {
-					fs.readFile(filePath, (err, data) => {
-						if (err) {
-							sendError(500, `Error finding ${request.url}`, err);
+			let suffix = path.extname(filePath);
+			const hasSuffix = (suffix.length > 1);
+			let suffixInMimeTypes = false;
+			if (hasSuffix) {
+				suffix = suffix.slice(1);
+				suffixInMimeTypes = (suffix in MIME_TYPES);
+			}
+			if (! hasSuffix || ! suffixInMimeTypes) {
+				sendError(403, `${request.url} is forbidden`);  // 5.
+			}
+			else {
+				fs.readFile(filePath, (err, data) => {
+					if (err) {
+						if (err.code === 'ENOENT') {
+							sendError(404, `Error ${request.url} not found`);  // 6.
 						}
 						else {
-							const mimeType = MIME_TYPES[suffix];
-							result.writeHead(200, { 'Content-Type': mimeType });
-							result.end(data);
+							sendError(500, `Error finding ${request.url}`, err);
 						}
-					});
-				}
+					}
+					else {
+						const mimeType = MIME_TYPES[suffix];
+						result.writeHead(200, { 'Content-Type': mimeType });  // 7.
+						result.end(data);
+					}
+				});
 			}
-		});
-	}
+		}
+	});
 });
 
 
