bfecf7282484e8a4696ac1dc2b059e2315ee38cc
brochristensen <78576381+brochristensen@users.noreply.github.com>
2025-10-21 10:45:23 -0600

    adding masaru fix

--- week06/week6.bat
+++ week06/week6.bat
@@ -1,5 +1,6 @@
 curl --ssl-no-revoke -O https://byui-cloud.github.io/itm101-course/week06/firestore.rules
 if not exist public mkdir public
+cd public
 curl --ssl-no-revoke -O https://byui-cloud.github.io/itm101-course/week06/cr.html
 curl --ssl-no-revoke -O https://byui-cloud.github.io/itm101-course/week06/crd.html
 curl --ssl-no-revoke -O https://byui-cloud.github.io/itm101-course/week06/crud.html
